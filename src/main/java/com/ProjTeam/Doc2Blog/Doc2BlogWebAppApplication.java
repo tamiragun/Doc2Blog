eceeb1f2f3f3cfbd335f49655f538487667f1762
Warren Bradley <warrenkb02@gmail.com>
2021-12-09 21:02:20 +0200

    Initial project setup.

---
+++ src/main/java/com/ProjTeam/Doc2Blog/Doc2BlogWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ProjTeam.Doc2Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doc2BlogWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Doc2BlogWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjTeam/Doc2Blog/Doc2BlogWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjTeam.Doc2Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doc2BlogWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
