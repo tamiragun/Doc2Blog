110c88e06a1e0dde94af24c230c6d50c9c8e7579
Warren Bradley <warrenkb02@gmail.com>
2022-01-25 20:54:13 +0200

    Commiting cleaner updated code for reminder backend.

--- src/main/java/com/ProjTeam/Doc2Blog/Reminders/RemindersCrudRep.java
+++ src/main/java/com/ProjTeam/Doc2Blog/Reminders/RemindersCrudRep.java
@@ -6,14 +6,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * RemindersCrudRep interface Class<br>
+ * This interface manages the CRUD repository for the Reminderss class and it's interaction with mysql.
+ *
+ * @author Warren Bradley
+ * @version 1.00, 21 Jan 2022
+ */
 // Creating an extension of the CrudRepository than can manage our Reminders model
 @Repository
 public interface RemindersCrudRep extends JpaRepository<Reminders, Integer>{
 	
+	//Method to find a list of reminders objects based in their acknowledged status.
 	public List<Reminders> findByAcknowledged(boolean acknowledged);
 	
+	//Method to find individual reminder objects using the project they are linked to.
 	public Reminders findByProject(Projects project);
 	
+	//Method to find individual reminder objects using their reminderId.
 	public Reminders findByReminderId(int reminderId);
 
 }
